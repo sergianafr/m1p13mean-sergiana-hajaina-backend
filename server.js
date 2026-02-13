b150b7640b805977e86cc267256aeb318e29ace3
sergianafr <sergiravelo@gmail.com>
2026-02-13 11:05:14 +0300

    create instruction for copilot

--- server.js
+++ server.js
@@ -12,16 +12,12 @@ const connectDB = require('./config/db');
 app.use(cors());
 app.use(express.json());
 
-
-
 app.use("/api/auth", require("./routes/auth.route"));
 app.use("/api/type-magasins", require("./routes/type-magasin.route"));
 app.use("/api/type-produits", require("./routes/type-produit.route"));
 app.use("/api/unites", require("./routes/unite.route"));
 app.use("/api/boxs", require("./routes/box.route"));
 app.use("/api/loyer-boxs", require("./routes/loyer-box.route"));
-
-
 app.use("/api/produits", require("./routes/produit.route"));
 app.use("/api/magasin-boxs", require("./routes/magasin-box.route"));
 
